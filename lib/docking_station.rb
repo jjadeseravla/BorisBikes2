59838a9eb237b5d3bcbf1c91f6e76864b1ccd564
jade <jadealvares@hotmail.co.uk>
2018-09-11 11:29:17 +0100

    refactored to follow single responsibility principle

--- lib/docking_station.rb
+++ lib/docking_station.rb
@@ -7,13 +7,23 @@ class DockingStation
   end
 
   def release_bike
-    fail 'No bikes available' if @bikes.empty?
+    fail 'No bikes available' if empty?
     @bikes.pop
   end
 
   def dock(bike)
-    fail 'Docking station is full' if @bikes.count >= 20
+    fail 'Docking station is full' if full?
     @bikes << bike #<< add to the end of this array
+  end
+
+  private
+
+  def full?
+    @bikes.count >= 20
+  end
+
+  def empty?
+    @bikes.empty?
   end
 
 end
